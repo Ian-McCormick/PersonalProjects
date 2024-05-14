927c40601d798875bd96ffeb554f2e858e0ff69b
Ian McCormick <mccormi@wwu.edu>
2024-05-13 20:41:59 -0700

    worked on encryption

--- Encryption/encryption.py
+++ Encryption/encryption.py
@@ -1,30 +1,41 @@
-def
+import tkinter as tk
+from tkinter import filedialog
+import os
 
-def new_listing(password):
-    name = input("Enter Name: ")
-    comp = input("Composition: ")
-    return
+CURRENT_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
 
-def print_entry(password):
-    return
+class Main:
+    def __init__(self):
+        self.root = tk.Tk()
+        #create labels
+        tk.Label(self.root, text="Password: ").grid(row=0, column=0)
+        tk.Label(self.root, text="File for decryption: ").grid(row=1, column=0)
 
-def try_int(input):
-    try:
-        num = int(input)
-    except ValueError:
-        num = -1
-    return num
+        #create entries
+        self.password = tk.Entry(self.root)
+        self.password.grid(row=0, column=1)
 
-def main():
-    pw = input("Enter Password: ")
-    while True:
-        raw = input("What do you want to do?\n1)New Entry\n2)Read Entry\n3)change password\n4)exit\nyour choice: ")
-        choice = try_int(raw)
-        if(choice == 1):
-            new_listing()
-        if(choice == 2):
-            print_entry()
-        if(choice == 3):
-            pw = input("Enter Password: ")
-        if (choice == 4):
-            break
+        self.fullFileName = ""
+        self.fileToView = tk.Label(self.root, text="")
+        fileButton = tk.Button(self.root, text = "Select File", command = lambda: self.changeFileToView())
+        fileButton.grid(row=1, column=2)
+
+        #create execution buttons
+        tk.Button(self.root, text="Add Entry", command= lambda: self.addEntry()).grid(row=2, column=0)
+        tk.Button(self.root, text="Decrypt Entry", command= lambda: self.decryptEntry()).grid(row=2, column=1)
+        self.root.mainloop()
+
+    def changeFileToView(self):
+        self.fullFileName = filedialog.askopenfilename(initialdir=CURRENT_DIRECTORY,
+                    title="Select Battle Map")
+        
+        self.fileToView.config(text = self.fullFileName.split("/")[-1])
+        self.root.update()
+
+    def addEntry(self):
+        return
+    
+    def decryptEntry(self):
+        return
+
+Main()
